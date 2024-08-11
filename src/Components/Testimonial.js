2b2cc738a2e2ac92d9d7ffa08b13500cfd4ffbc1
Sabbir <sabbirhossainruet@gmail.com>
2024-08-11 12:56:13 +0600

    Revised

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -10,7 +10,7 @@ const Testimonial = () => {
         <p className="primary-subheading">Testimonial</p>
         <h1 className="primary-heading">What They Are Saying</h1>
         <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          Lorem ipsum dolor sit amet consectrfd. Non tincidunt magna non et
           elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
         </p>
       </div>
